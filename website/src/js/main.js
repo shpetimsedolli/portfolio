ba52a54ab6733885994815a3f96c013f3773ebcd
shpetimsedolli <shpeti101@gmail.com>
2020-10-05 18:49:59 +0200

    first commit

---
+++ website/src/js/main.js
@@ -0,0 +1,16 @@
+(function($, window, document) {
+  "use strict";
+  $(function() {
+    //##Variables
+    var $body = $("body"),
+      $window = $(window),
+      $doc = $(document),
+      defaultEasing = [0.4, 0.0, 0.2, 1];
+    //End Variables
+
+    console.log(
+      "%c Developed by: Kutia Software Company http://kutia.net",
+      "padding:9px 5px; background:#3c4952; line-height:25px;color: #59bf9d"
+    );
+  });
+})(window.jQuery, window, document);
---
+++ website/src/js/scripts.js
@@ -0,0 +1,4 @@
+window.jQuery = require("./jquery-1.12.4");
+require("./modernizr");
+require("./cssbrowserselector");
+require("./main");
